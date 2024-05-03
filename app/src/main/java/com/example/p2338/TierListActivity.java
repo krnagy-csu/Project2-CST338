8f48158ebb8325ecce5e3eb996b63823bb87c96c
krisztiannagy <krnagy@csumb.edu>
2024-05-03 15:29:44 -0700

    Made the path to the actual tier list maker; fixed a bunch of minor issues; made admin status carry over between all the different activities; set up the working layout for the tier list itself; set up basic layouts for each activity, and added them to AndroidManifest.xml
    BUGS:
    -Every single thing except the title itself on activity_tierlist.xml just disappears when running TierListActivity.java
    
    TODO:
    -Finish out admin options menu
    -Make the tier actually work
    -Reconsider drag&drop - might be more trouble than it's worth, maybe better to just go "tap the thingy you want to move, then tap the tier you want it in"
    
    COMMENTS:
    Should be pretty doable. Will keep working on it a while longer today, but will work on things other than the tier list itself - it scares me.

---
+++ app/src/main/java/com/example/p2338/TierListActivity.java
@@ -0,0 +1,88 @@
+package com.example.p2338;
+
+import static androidx.core.view.ViewCompat.startDragAndDrop;
+
+import android.graphics.Point;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.DragStartHelper;
+
+import java.util.ArrayList;
+
+public class TierListActivity extends AppCompatActivity {
+    boolean isAdmin = false;
+    Integer topic = 0;
+    String TAG = "TIERLIST_ACTIVITY";
+    ArrayList<View> draggables = new ArrayList<View>();
+    ArrayList<View> tierImages = new ArrayList<View>();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle extras = getIntent().getExtras();
+        String title;
+
+        setContentView(R.layout.activity_tierlist);
+        try{
+            isAdmin=extras.getBoolean("Admin");
+        }catch (NullPointerException e){
+            Log.e(TAG,"Failed to get/set admin status. Defaulting to &quot;false%quot;");
+        }
+
+        populateDraggables();
+        populateTierImages();
+
+        TextView titleText =  findViewById(R.id.tierListTitleTextView);
+        try {
+            topic = extras.getInt("Topic");
+        } catch (Exception e){
+            Log.e(TAG,"Failed to get topic.");
+        }
+        if (topic == 0){
+            title = "Placeholder";
+        } else if(topic == 1){
+            title = "Topic1";
+        } else if(topic == 2){
+            title = "Topic2";
+        } else {
+            title = "Topic3";
+        }
+
+        titleText.setText(title);
+
+        DragStartHelper.OnDragStartListener listener = new DragStartHelper.OnDragStartListener() {
+            @Override
+            public boolean onDragStart(@NonNull View v, @NonNull DragStartHelper helper) {
+                return false;
+            }
+        };
+    }
+
+    private void populateDraggables(){
+        draggables.add(findViewById(R.id.TLDraggable1));
+        draggables.add(findViewById(R.id.TLDraggable2));
+        draggables.add(findViewById(R.id.TLDraggable3));
+        draggables.add(findViewById(R.id.TLDraggable4));
+        draggables.add(findViewById(R.id.TLDraggable5));
+        draggables.add(findViewById(R.id.TLDraggable6));
+        draggables.add(findViewById(R.id.TLDraggable7));
+        draggables.add(findViewById(R.id.TLDraggable8));
+        draggables.add(findViewById(R.id.TLDraggable9));
+        draggables.add(findViewById(R.id.TLDraggable10));
+    }
+
+    private void populateTierImages(){
+        tierImages.add(findViewById(R.id.STierImage));
+        tierImages.add(findViewById(R.id.ATierImage));
+        tierImages.add(findViewById(R.id.BTierImage));
+        tierImages.add(findViewById(R.id.CTierImage));
+        tierImages.add(findViewById(R.id.DTierImage));
+        tierImages.add(findViewById(R.id.FTierImage));
+    }
+}
--- app/src/main/java/com/example/p2338/TopicSelectionActivity.java
+++ app/src/main/java/com/example/p2338/TopicSelectionActivity.java
@@ -1,15 +1,65 @@
 package com.example.p2338;
 
+import android.adservices.topics.Topic;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class TopicSelectionActivity extends AppCompatActivity {
+    Intent topicSelIntent;
+    String TAG = "TOPIC_ACTIVITY";
+    boolean isAdmin = false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topicsel);
 
+        try{
+            isAdmin = getIntent().getExtras().getBoolean("Admin");
+        } catch (NullPointerException e){
+            Log.e(TAG,"Unable to get Admin status, defaulting to false.");
+            isAdmin = false;
+        }
+        Button button1 = findViewById(R.id.topicselbutton1);
+        Button button2 = findViewById(R.id.topicselbutton2);
+        Button button3 = findViewById(R.id.topicselbutton3);
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                topicSelIntentFactory(TopicSelectionActivity.this,1);
+                startActivity(topicSelIntent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                topicSelIntentFactory(TopicSelectionActivity.this,2);
+                startActivity(topicSelIntent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                topicSelIntentFactory(TopicSelectionActivity.this,3);
+                startActivity(topicSelIntent);
+            }
+        });
+    }
+
+
+
+    private void topicSelIntentFactory (Context context, int topic){
+        topicSelIntent = new Intent(context, TierListActivity.class);
+        topicSelIntent.putExtra("Topic",topic);
+        topicSelIntent.putExtra("Admin",isAdmin);
     }
 }
