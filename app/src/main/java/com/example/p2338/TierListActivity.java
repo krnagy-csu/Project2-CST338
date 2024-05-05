dca8ace903fb4fa0320817d6492bd03292531d26
krisztiannagy <krnagy@csumb.edu>
2024-05-05 14:36:53 -0700

    Tierlist works.

--- app/src/main/java/com/example/p2338/TierListActivity.java
+++ app/src/main/java/com/example/p2338/TierListActivity.java
@@ -3,15 +3,20 @@ package com.example.p2338;
 import static androidx.core.view.ViewCompat.startDragAndDrop;
 
 import android.graphics.Point;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.view.DragStartHelper;
 
 import java.util.ArrayList;
@@ -19,9 +24,16 @@ import java.util.ArrayList;
 public class TierListActivity extends AppCompatActivity {
     boolean isAdmin = false;
     Integer topic = 0;
+    Integer highlighted = 10;
     String TAG = "TIERLIST_ACTIVITY";
-    ArrayList<View> draggables = new ArrayList<View>();
+    ArrayList<ImageButton> tierItems = new ArrayList<ImageButton>();
     ArrayList<View> tierImages = new ArrayList<View>();
+
+    ArrayList <ImageButton> tierBacks = new ArrayList<ImageButton>();
+
+    String[] tiers = {"","","","","",""};
+    Boolean[] placed = {false,false,false,false,false,false,false,false,false,false};
+    
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +47,7 @@ public class TierListActivity extends AppCompatActivity {
             Log.e(TAG, "Failed to get/set admin status. Defaulting to &quot;false%quot;");
         }
 
-        populateDraggables();
+        populateTierItems();
         populateTierImages();
 
         TextView titleText = findViewById(R.id.tierListTitleTextView);
@@ -56,42 +68,77 @@ public class TierListActivity extends AppCompatActivity {
 
         titleText.setText(title);
 
-        for (View v : draggables){
-            v.setActivated(true);
-            v.setVisibility(View.VISIBLE);
+        populateTierBacks();
+
+        for (ImageButton tierBack : tierBacks) {
+            tierBack.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int tier = tierBacks.indexOf(tierBack);
+                    Log.i(TAG,"Pressed button for the " + tier + " tier");
+                    if (highlighted != 10){
+                        if (!placed[highlighted]) {
+                            tiers[tier] += String.format("%s", highlighted);
+                            placed[highlighted] = true;
+                            ImageButton target = tierItems.get(highlighted);
+                            target.setX(v.getX()-150f + (tiers[tier].length() * 180f));
+                            target.setY(v.getY()+25);
+                            highlighted = 10;
+                        }
+                    }
+                    Log.i(TAG,"Tier " + tier + " is now " + tiers[tier]);
+                }
+            });
         }
 
+        for (ImageButton tierItem : tierItems){
+            tierItem.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    int item = tierItems.indexOf(tierItem);
+                    Log.i(TAG,"Pressed the " + item + " item");
+                    if (!placed[item]) {
+                        highlighted = item;
+                    }
+                    ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams) v.getLayoutParams();
+                    ConstraintSet c = new ConstraintSet();
+                    // c.clone(TierListActivity.this,tierItem.getId());
+                }
+            });
+        }
 
-
-        DragStartHelper.OnDragStartListener listener = new DragStartHelper.OnDragStartListener() {
-            @Override
-            public boolean onDragStart(@NonNull View v, @NonNull DragStartHelper helper) {
-                return false;
-            }
-        };
     }
 
-    private void populateDraggables(){
-        /*
-        draggables.add(findViewById(R.id.TLDraggable1));
-        draggables.add(findViewById(R.id.TLDraggable2));
-        draggables.add(findViewById(R.id.TLDraggable3));
-        draggables.add(findViewById(R.id.TLDraggable4));
-        draggables.add(findViewById(R.id.TLDraggable5));
-        draggables.add(findViewById(R.id.TLDraggable6));
-        draggables.add(findViewById(R.id.TLDraggable7));
-        draggables.add(findViewById(R.id.TLDraggable8));
-        draggables.add(findViewById(R.id.TLDraggable9));
-        draggables.add(findViewById(R.id.TLDraggable10);
-         */
+    private void populateTierItems(){
+        
+        tierItems.add(0,findViewById(R.id.TLItem0));
+        tierItems.add(1,findViewById(R.id.TLItem1));
+        tierItems.add(2,findViewById(R.id.TLItem2));
+        tierItems.add(3,findViewById(R.id.TLItem3));
+        tierItems.add(4,findViewById(R.id.TLItem4));
+        tierItems.add(5,findViewById(R.id.TLItem5));
+        tierItems.add(6,findViewById(R.id.TLItem6));
+        tierItems.add(7,findViewById(R.id.TLItem7));
+        tierItems.add(8,findViewById(R.id.TLItem8));
+        tierItems.add(9,findViewById(R.id.TLItem9));
+         
     }
 
     private void populateTierImages(){
-        tierImages.add(findViewById(R.id.tierSimg));
-        tierImages.add(findViewById(R.id.tierAimg));
-        tierImages.add(findViewById(R.id.tierBimg));
-        tierImages.add(findViewById(R.id.tierCimg));
-        tierImages.add(findViewById(R.id.tierDimg));
-        tierImages.add(findViewById(R.id.tierFimg));
+        tierImages.add(0,findViewById(R.id.tierSimg));
+        tierImages.add(1,findViewById(R.id.tierAimg));
+        tierImages.add(2,findViewById(R.id.tierBimg));
+        tierImages.add(3,findViewById(R.id.tierCimg));
+        tierImages.add(4,findViewById(R.id.tierDimg));
+        tierImages.add(5,findViewById(R.id.tierFimg));
+    }
+
+    private void populateTierBacks(){
+        tierBacks.add(0,findViewById(R.id.bgStier));
+        tierBacks.add(1,findViewById(R.id.bgAtier));
+        tierBacks.add(2,findViewById(R.id.bgBtier));
+        tierBacks.add(3,findViewById(R.id.bgCtier));
+        tierBacks.add(4,findViewById(R.id.bgDtier));
+        tierBacks.add(5,findViewById(R.id.bgFtier));
     }
 }
